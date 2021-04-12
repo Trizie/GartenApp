eeb4988a4fefc3d918d24ce2f9a67e1280a8ac78
Trizie <patrizia.stutzenstein@gmx.at>
2021-04-12 09:24:41 +0200

    Qt Steuerungssoftware

---
+++ GartenProg/chartanzeige.h
@@ -0,0 +1,14 @@
+#ifndef CHARTANZEIGE_H
+#define CHARTANZEIGE_H
+
+#include <QtSql>
+#include <QtGui>
+#include <QDialog>
+
+class chartanzeige : public QDialog
+{
+public:
+    chartanzeige();
+};
+
+#endif // CHARTANZEIGE_H
---
+++ GartenProg/listenanzeige.h
@@ -0,0 +1,15 @@
+#ifndef LISTENANZEIGE_H
+#define LISTENANZEIGE_H
+
+#include <QtSql>
+#include <QtGui>
+#include <QDialog>
+
+
+class listenanzeige : public QDialog
+{
+public:
+    listenanzeige();
+};
+
+#endif // LISTENANZEIGE_H
